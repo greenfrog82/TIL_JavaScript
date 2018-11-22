e26bce8f0bb22b64e7a861119a8ce76db82af011
greenfrog82 <greenfrog82@naver.com>
2018-11-22 12:19:41 +0900

    React TODO List - add TodoFilter and add react-native-elements, redux and redux-react in package.json

---
+++ ReactNative/tutorial/AwesomeProject/Components/TodoInput.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { StyleSheet, TextInput, Button, View } from 'react-native';
+
+export default class TodoInput extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput placeholder='Type here your todo item'></TextInput> 
+        <Button title="Click" onPress={() => alert('test')}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    // alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
